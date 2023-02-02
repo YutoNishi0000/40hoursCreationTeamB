c1acb2d6d19537111de66e902720120ba6ee792d
YutoNishi0000 <yuto0324n@gmail.com>
2023-02-02 22:53:51 +0900

    FPS移動

--- Assets/YutoNishimura/Scripts/Camera/CameraController.cs
+++ Assets/YutoNishimura/Scripts/Camera/CameraController.cs
@@ -3,61 +3,114 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class CameraController : Human
 {
-    private const float YAngle_MIN = -30.0f;   //カメラのY方向の最小角度
-    private const float YAngle_MAX = 20.0f;     //カメラのY方向の最大角度
+    Vector3 moveDirection = Vector3.zero;
+    Vector3 targetDirection;        //移動する方向のベクトル
+    private CharacterController controller;
+    float x, z;
+    float speed = 3f;
 
-    public Transform target;    //追跡するオブジェクトのtransform
-    public Vector3 offset;      //追跡対象の中心位置調整用オフセット
-    private Vector3 lookAt;     //targetとoffsetによる注視する座標
+    public GameObject cam;
+    Quaternion cameraRot, characterRot;
+    float Xsensityvity = 3f, Ysensityvity = 3f;
 
-    private float distance = 5.0f;    //キャラクターとカメラ間の角度
-    private float DEFAULT_DISTANCE = 3.0f;
-    private float distance_min = 0.0f;  //キャラクターとの最小距離
-    private float distance_max = 5.0f; //キャラクターとの最大距離
-    private float currentX = 0.0f;  //カメラをX方向に回転させる角度
-    private float currentY = 0.0f;  //カメラをY方向に回転させる角度
+    bool cursorLock = true;
 
-    //カメラ回転用係数(値が大きいほど回転速度が上がる)
-    private float moveX = 4.0f;     //マウスドラッグによるカメラX方向回転係数
-    private float moveY = 2.0f;     //マウスドラッグによるカメラY方向回転係数
-    private float moveX_QE = 2.0f;  //QEキーによるカメラX方向回転係数
-
-    [SerializeField] private float CAMERA_SENSITIVILITY = 0.1f;
+    //変数の宣言(角度の制限用)
+    float minX = -40f, maxX = 40f;
 
     // Start is called before the first frame update
     void Start()
     {
-        currentX += 180;
-        target = playerInstance.transform;
+        cameraRot = cam.transform.localRotation;
+        characterRot = transform.localRotation;
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //マウス右クリックを押しているときだけマウスの移動量に応じてカメラが回転
-        if (Input.GetMouseButton(1))
+        float xRot = Input.GetAxis("Mouse X") * Ysensityvity;
+        float yRot = Input.GetAxis("Mouse Y") * Xsensityvity;
+
+        cameraRot *= Quaternion.Euler(-yRot, 0, 0);
+        characterRot *= Quaternion.Euler(0, xRot, 0);
+
+        //Updateの中で作成した関数を呼ぶ
+        cameraRot = ClampRotation(cameraRot);
+
+        cam.transform.localRotation = cameraRot;
+        transform.localRotation = characterRot;
+
+        UpdateCursorLock();
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    public override void MoveControl()
+    {
+        //キーボード入力を取得
+        float v = Input.GetAxisRaw("Vertical");         //InputManagerの↑↓の入力       
+        float h = Input.GetAxisRaw("Horizontal");       //InputManagerの←→の入力 
+
+        //カメラの正面方向ベクトルからY成分を除き、正規化してキャラが走る方向を取得
+        Vector3 forward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 right = Camera.main.transform.right; //カメラの右方向を取得
+
+        //カメラの方向を考慮したキャラの進行方向を計算
+        targetDirection = h * right + v * forward;
+
+        moveDirection = Vector3.Scale(targetDirection, new Vector3(1, 0, 1)).normalized;
+        moveDirection *= speed;
+
+        //最終的な移動処理
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    public void UpdateCursorLock()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            currentX += Input.GetAxis("Mouse X") * moveX;
-            currentY += Input.GetAxis("Mouse Y") * moveY;
-            currentY = Mathf.Clamp(currentY, YAngle_MIN, YAngle_MAX);
+            cursorLock = false;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            cursorLock = true;
+        }
+
+
+        if (cursorLock)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else if (!cursorLock)
+        {
+            Cursor.lockState = CursorLockMode.None;
         }
     }
 
-    private void LateUpdate()
+    //角度制限関数の作成
+    public Quaternion ClampRotation(Quaternion q)
     {
-        if (target != null)  //targetが指定されるまでのエラー回避
-        {
-            var targetVec = new Vector3(target.position.x, 0, target.position.z) + offset;
-            lookAt = target.position + offset;  //注視座標はtarget位置+offsetの座標
+        //q = x,y,z,w (x,y,zはベクトル（量と向き）：wはスカラー（座標とは無関係の量）)
 
-            //カメラ旋回処理
-            Vector3 dir = new Vector3(0, 0, -distance);
-            Quaternion rotation = Quaternion.Euler(-currentY, currentX, 0);
-            transform.position = lookAt + rotation * dir;   //カメラの位置を変更
-            transform.LookAt(lookAt);   //カメラをLookAtの方向に向けさせる
-        }
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1f;
+
+        float angleX = Mathf.Atan(q.x) * Mathf.Rad2Deg * 2f;
+
+        angleX = Mathf.Clamp(angleX, minX, maxX);
+
+        q.x = Mathf.Tan(angleX * Mathf.Deg2Rad * 0.5f);
+
+        return q;
     }
 }
