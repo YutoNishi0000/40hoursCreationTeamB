d4c8b128fe5c3417c7fabcdb802fee1d3e7b38e6
YutoNishi0000 <yuto0324n@gmail.com>
2023-02-02 22:53:21 +0900

    Revert "ブランチのセットアップ"
    This reverts commit aece2c303b65a18b5ccfa594207f21cd49ed01ca.

---
+++ Assets/Takahashi_Kyoya/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    /// <summary>
+    /// Ÿ‚Ì“ú‚És‚­
+    /// </summary>
+    /// <param name="sceneName"></param>
+    /// <param name="feeling"></param>
+    public void NextDay(string sceneName, bool canNextDay)
+    {
+        if (canNextDay)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
