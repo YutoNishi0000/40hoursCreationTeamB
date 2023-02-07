a1929182761153f1b3b3d368312fc48cd2dbccf5
YutoNishi0000 <yuto0324n@gmail.com>
2023-02-08 04:44:17 +0900

    制限時間の実装

---
+++ Assets/YutoNishimura/Scripts/Time/TimeController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//ゲーム内の時間を制御する関数
+public class TimeController : MonoBehaviour
+{
+    private bool _isTimePassed;
+    public Text timeText;
+    [SerializeField] private float TimeLeft = 120;      //残り時間
+    private float _time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _isTimePassed = false;
+        _time = TimeLeft;
+        OnTime(TimeLeft, timeText);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _time -= Time.deltaTime;
+        OnTime(_time, timeText);
+    }
+
+    //残り時間を表示
+    void OnTime(float leftTime, Text time)
+    {
+        if(leftTime <= 0)
+        {
+            _isTimePassed = true;
+            time.text = "TIME OVER...";
+            return;
+        }
+
+        time.text = "残り" + Mathf.FloorToInt(leftTime / 60) + "分" + Mathf.FloorToInt(leftTime % 60) + "秒";
+    }
+
+    public bool GetTimePassedFlag()
+    {
+        return _isTimePassed;
+    }
+}
