ac95adc009ecc8e6a8eea9efdb911530d7ff0e28
YutoNishi0000 <yuto0324n@gmail.com>
2023-02-07 11:49:52 +0900

    警官の動き実装

--- Assets/YutoNishimura/Prefabs/Prefabs/Police/PoliceController.cs
+++ Assets/YutoNishimura/Prefabs/Prefabs/Police/PoliceController.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,12 +7,21 @@ using UnityEngine.UI;
 //プレイヤーが察知範囲内に居たらゲージを減らして行く
 public class PoliceController : MonoBehaviour
 {
-    private bool _policeCheckupFlag;            //職質危険察知フラグ
-    public Slider _dangerDetectionGauge;        //危険度察知ゲージ
+    private static bool _policeCheckupFlag;            //職質危険察知フラグ
+    public Slider[] _dangerDetectionGauge;        //危険度察知ゲージ
+    private readonly float CAUGHT_TIME = 4;
+    private float time;
 
     // Start is called before the first frame update
     void Start()
     {
+        time = 0;
+
+        for(int i = 0; i < _dangerDetectionGauge.Length; i++)
+        {
+            _dangerDetectionGauge[i].maxValue = CAUGHT_TIME;
+        }
+
         _policeCheckupFlag = false;
 
         if(_dangerDetectionGauge == null)
@@ -23,21 +33,42 @@ public class PoliceController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //職質危険察知フラグがオンだった場合は
+        if (_policeCheckupFlag)
+        {
+            Debug.Log("プレイヤーを職質しようかな・・・");
+            for (int i = 0; i < _dangerDetectionGauge.Length; i++)
+            {
+                _dangerDetectionGauge[i].value -= Time.deltaTime;
+            }
+        }
+        //職質危険察知フラグがオフだった場合は
+        else
+        {
+            //ゲージの値をもとに戻す
+            for (int i = 0; i < _dangerDetectionGauge.Length; i++)
+            {
+                _dangerDetectionGauge[i].DOValue(_dangerDetectionGauge[i].maxValue, CAUGHT_TIME / 3);
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        //もしもプレイヤーが察知範囲内に進入してきたら
         if(other.gameObject.CompareTag("Player"))
         {
+            //フラグをオンにする
             _policeCheckupFlag = true;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if(other.gameObject.CompareTag("Player"))
+        //もしもプレイヤーが察知範囲内に進入してきたら
+        if (other.gameObject.CompareTag("Player"))
         {
+            //フラグをオフにする
             _policeCheckupFlag = false;
         }
     }
