c95f159a7ba20796239bd003a217dd39745884e6
DESKTOP-P67ANVU\kyoya <kyoya151101@icloud.com>
2023-02-12 16:25:11 +0900

    拍動animation完成と置き換え完了

--- Assets/Takahashi_Kyoya/Scripts/UI/HeartBeat.cs
+++ Assets/Takahashi_Kyoya/Scripts/UI/HeartBeat.cs
@@ -5,92 +5,17 @@ using UnityEngine.UI;
 
 public class HeartBeat : MonoBehaviour
 {
-    //大きくなるスピード
-    private float largeSpeed = 0.01f;
-    //小さくなるスピード
-    private float smallSpeed = 0.01f;
-    //止まる時間
-    private float stopTime = 0.01f;
-    //止まる時間の経過時間
-    private float time;
-    //最小の大きさ
-    private Vector3 minSize = new Vector3(0.4f, 0.4f, 0.4f);
-    //最大の大きさ
-    private Vector3 maxSize = new Vector3(0.7f, 0.7f, 0.7f);
-    private enum STATE
+    Animator animator;
+    private void Awake()
     {
-        large,
-        stop,
-        small,
+        animator = GetComponent<Animator>();
     }
-    private STATE state;
-
-    private void Update()
+    public void FastHeartBeat()
     {
-        BeatUpdate();
-        FastBeat();
+        animator.SetBool("IsFast", true);
     }
-
-    /// <summary>
-    /// UIを拍動させる
-    /// </summary>
-    void BeatUpdate()
+    public void IdleHeartBeat()
     {
-        //大きくなって少し止まって小さくなる
-        switch (state)
-        {
-            case STATE.large:
-                LargeUpdate();
-                break;
-            case STATE.stop:
-                StopUpdate();
-                break;
-            case STATE.small:
-                SmallUpdate();
-                break;
-        }
-    }
-
-    /// <summary>
-    /// 大きくする
-    /// </summary>
-    void LargeUpdate()
-    {
-        this.transform.localScale += new Vector3(largeSpeed, largeSpeed, largeSpeed);
-        if (this.transform.localScale.x > maxSize.x)
-        {
-            state = STATE.stop;
-            time = Time.time;
-        }
-    }
-    /// <summary>
-    /// サイズ変更を止める
-    /// </summary>
-    void StopUpdate()
-    {
-        if (Time.time - time > stopTime)
-        {
-            state = STATE.small;
-        }
-    }
-    /// <summary>
-    /// 小さくする
-    /// </summary>
-    void SmallUpdate()
-    {
-        this.transform.localScale -= new Vector3(smallSpeed, smallSpeed, smallSpeed);
-        if (this.transform.localScale.x < minSize.x)
-        {
-            state = STATE.large;
-        }
-    }
-    /// <summary>
-    /// 拍動を早くする
-    /// </summary>
-    void FastBeat()
-    {
-        largeSpeed = 0.005f + ((RayTest.lockonTime / 2.0f) / 1000);
-        smallSpeed = 0.005f + ((RayTest.lockonTime / 2.0f) / 1000);
-        stopTime = 0.1f - (((RayTest.lockonTime * 5.0f) / 3.0f) / 100);
+        animator.SetBool("IsFast", false);
     }
 }
