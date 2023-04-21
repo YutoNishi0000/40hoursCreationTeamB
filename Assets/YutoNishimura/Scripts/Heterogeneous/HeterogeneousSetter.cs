07c39104f16f38482ce13eb13f1396252f7cc235
YutoNishi0000 <yuto0324n@gmail.com>
2023-04-21 13:08:08 +0900

    異質なもの実装途中

---
+++ Assets/YutoNishimura/Scripts/Heterogeneous/HeterogeneousSetter.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//ˆÙ¿‚È‚à‚Ì
+public class HeterogeneousSetter : MonoBehaviour
+{
+    [SerializeField] private GameObject[] points;   //Œ»“_‚Å‚Í8‰ÓŠ‚Æ‰¼’è
+    [SerializeField] private GameObject ObjectA;
+    [SerializeField] private GameObject ObjectB;
+    [SerializeField] private GameObject ObjectC;
+    [SerializeField] private GameObject ObjectD;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
