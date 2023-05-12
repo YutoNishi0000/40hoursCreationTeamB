1f5ff04515aec63db280ee9ad02e79ec2a808b6b
madarame0610 <112837572+madarame0610@users.noreply.github.com>
2023-05-12 11:54:23 +0900

    UI追加

---
+++ Assets/Madarame/Spripts/SkillCount.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillCount : MonoBehaviour
+{
+    // •\¦/”ñ•\¦‚·‚éUI
+    [SerializeField] GameObject SkillCount0;
+    [SerializeField] GameObject SkillCount1;
+    [SerializeField] GameObject SkillCount2;
+    // UIØ‚è‘Ö‚¦—p•Ï”
+    int[] _SkillCount;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
