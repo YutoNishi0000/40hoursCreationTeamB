c42ec18b20e708e5885cefedcf884093081625d8
madarame0610 <112837572+madarame0610@users.noreply.github.com>
2023-05-12 12:10:53 +0900

    UI少し修正

--- Assets/Madarame/Spripts/SkillCount.cs
+++ Assets/Madarame/Spripts/SkillCount.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SkillCount : MonoBehaviour
 {
@@ -9,15 +10,21 @@ public class SkillCount : MonoBehaviour
     [SerializeField] GameObject SkillCount1;
     [SerializeField] GameObject SkillCount2;
     // UIêÿÇËë÷Ç¶ópïœêî
-    int[] _SkillCount;
-
-    private void Start()
-    {
-        
-    }
+    int[] _SkillCount = new int[4];
 
     private void Update()
     {
-        
+        if (_SkillCount[1] == 1)
+        {
+            SkillCount0.gameObject.SetActive(true);
+        }
+        if (_SkillCount[2] == 2)
+        {
+            SkillCount1.gameObject.SetActive(true);
+        }
+        if (_SkillCount[3] == 3)
+        {
+            SkillCount2.gameObject.SetActive(true);
+        }
     }
 }
