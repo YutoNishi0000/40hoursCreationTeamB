eca83cce62d8781e61acd8f17470742c2c45aacb
TKyoya12 <kyoya151101123@gmail.com>
2023-05-19 14:27:48 +0900

    スキルUI作成開始

---
+++ Assets/Kyoya_Takahashi/Script/UI/SkillUi.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillUi : MonoBehaviour
+{
+    [SerializeField] private GameObject speed;
+    [SerializeField] private GameObject score;
+    [SerializeField] private GameObject target;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
