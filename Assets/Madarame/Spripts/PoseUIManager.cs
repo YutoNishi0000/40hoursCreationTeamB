7fc61021a0faeec2377d3a355cd123874ecbbe39
madarame0610 <112837572+madarame0610@users.noreply.github.com>
2023-06-09 12:21:23 +0900

    ポーズ画面制作途中

---
+++ Assets/Madarame/Spripts/PoseUIManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PoseUIManager : MonoBehaviour
+{
+    /// <summary>選択肢ボタン </summary>    
+    private Button _button;
+
+    /// <summary>イメージ </summary>    
+    private Image _image;
+
+    /// <summary>テキスト </summary>    
+    private Text _text;
+
+    /// <summary>ボタンクラスを取得 </summary>
+    public Button Button => _button;
+    
+    /// <summary>イメージクラスを取得 </summary>
+    public Image Image => _image;
+
+    private void Start()
+    {
+        _button = GetComponent<Button>();
+        _image　= GetComponent<Image>();
+        _text = transform.GetChild(0).GetComponent<Text>();
+    }
+    /// <summary>UI表示</summary>
+    public void ShowUI()
+    {
+        _button.enabled = true;
+        _image.enabled = true;
+        _text.enabled = true;
+    }
+    /// <summary>UI非表示</summary>
+    public void HideUI()
+    {
+        _button.enabled = false;
+        _image.enabled = false;
+        _text.enabled = false;
+    }
+}
--- Assets/Madarame/Spripts/SelectUI.cs
+++ Assets/Madarame/Spripts/SelectUI.cs
@@ -1,18 +1,94 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SelectUI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /// <summary>ポーズ中かどうか </summary>
+    public bool _isPosing = false;
+
+    /// <summary>背景 </summary>    
+    [Header("背景"), SerializeField]
+    private PoseUIManager _background;
+
+    /// <summary>セレクトボタン </summary>    
+    [Header("セレクトボタン"), SerializeField]
+    private PoseUIManager _selectButton;
+    
+    /// <summary>リスタートボタン </summary>    
+    [Header("リスタートボタン"), SerializeField]
+    private PoseUIManager _reStartButton;
+    
+    /// <summary>オプションボタン </summary>    
+    [Header("オプションボタン"), SerializeField]
+    private PoseUIManager _optionButton;
+
+    /// <summary>セレクトシーンのインデックス番号</summary>
+    private const int SelectSceneIndex = 2;
+
+    /// <summary>ゲームシーンのインデックス番号</summary>
+    private const int ReStartSceneIndex = 3;
+    
+    /// <summary>オプションシーンのインデックス番号</summary>
+    private const int OptionSceneIndex = 6;
+
     void Start()
     {
-        
+        if (_selectButton == null || _reStartButton == null || _optionButton == null)
+        {
+            Debug.LogError("selectUIのインスペクターにButtonを格納してください。");
+            return;
+        }
+
+        Debug.Log("a");
+        HidePoseUI();
+
+        // ボタンクリック時にイベント追加 ---------------------- //
+        _selectButton. Button.onClick.AddListener(SelectMove);
+        _reStartButton.Button.onClick.AddListener(ReStartMove);
+        _optionButton. Button.onClick.AddListener(OptionMove);
+        // ----------------------------------------------------- //
+    }
+    private void Update()
+    {
+        if(Input.GetButtonDown("Space"))
+        {
+            _isPosing = true;
+            GameManager.Instance.IsPlayGame = false;
+            ShowPoseUI();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary> ポーズ画面非表示 </summary>
+    private void HidePoseUI()
     {
-        
+        _background.HideUI();
+        _selectButton.HideUI();
+        _reStartButton.HideUI();
+        _optionButton.HideUI();
+    }
+    /// <summary> ポーズ画面表示 </summary>
+    private void ShowPoseUI()
+    {
+        _background.ShowUI();
+        _selectButton.ShowUI();
+        _reStartButton.ShowUI();
+        _optionButton.ShowUI();
+    }
+    /// <summary> セレクト画面に移動 </summary>
+    private void SelectMove()
+    {
+        SceneManager.LoadScene(SelectSceneIndex);
+    }
+    /// <summary> リスタートする </summary>
+    private void ReStartMove()
+    {
+        SceneManager.LoadScene(ReStartSceneIndex);
+    }
+    /// <summary> オプション画面に移動 </summary>
+    private void OptionMove()
+    {
+        SceneManager.LoadScene(OptionSceneIndex);
     }
 }
