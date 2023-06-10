b2e81e6a8ba449dacc90daee2526d5db2c7ff6df
madarame0610 <112837572+madarame0610@users.noreply.github.com>
2023-06-10 17:35:42 +0900

    オプション制作途中

--- Assets/Kyoya_Takahashi/Script/InGame/UI/Pose.cs
+++ Assets/Kyoya_Takahashi/Script/InGame/UI/Pose.cs
@@ -1,23 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Pose : MonoBehaviour
 {
-    private const int childNum = 4;     //子オブジェクトの数
-    private GameObject[] poseUI = new GameObject[childNum]; //UIのオブジェクト情報
+    /// <summary>セレクトボタン </summary>    
+    [Header("セレクトボタン"), SerializeField]
+    private PoseUIManager _selectButton;
+
+    /// <summary>リスタートボタン </summary>    
+    [Header("リスタートボタン"), SerializeField]
+    private PoseUIManager _reStartButton;
+
+    /// <summary>オプションボタン </summary>    
+    [Header("オプションボタン"), SerializeField]
+    private PoseUIManager _optionButton;
+
+    /// <summary>セレクトシーンのインデックス番号</summary>
+    [Header("セレクトシーンのインデックス番号"), SerializeField]
+    private int SelectSceneIndex;
+
+    /// <summary>ゲームシーンのインデックス番号</summary>
+    [Header("ゲームシーンのインデックス番号"), SerializeField]
+    private int ReStartSceneIndex;
+
+    /// <summary>オプションシーンのインデックス番号</summary>
+    [Header("オプションシーンのインデックス番号"), SerializeField]
+    private int OptionSceneIndex;
+
+    //子オブジェクトの数
+    private const int childNum = 4;   
+    
+    //UIのオブジェクト情報
+    private GameObject[] poseUI = new GameObject[childNum];
+
+    //ポーズ中かどうか
     private bool IsPosing = false;
-    void Start()
+
+    private void Start()
     {
+        if (_selectButton == null || _reStartButton == null || _optionButton == null)
+        {
+            Debug.LogError("selectUIのインスペクターにButtonを格納してください。");
+            return;
+        }
+
         for (int i = 0; i < poseUI.Length; i++)
         {
             poseUI[i] = transform.GetChild(i).gameObject;
         }
+
         hideUI();
+
+        // ボタンクリック時にイベント追加 ---------------------- //
+        _selectButton.Button.onClick.AddListener(SelectMove);
+        _reStartButton.Button.onClick.AddListener(ReStartMove);
+        _optionButton.Button.onClick.AddListener(OptionMove);
+        // ----------------------------------------------------- //
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         // ===== ポーズ中の処理 =====
         if (IsPosing)
@@ -36,6 +79,7 @@ public class Pose : MonoBehaviour
             IsPosing = true;
         }
     }
+    /// <summary> ポーズ画面非表示 </summary>
     private void hideUI()
     {
         for (int i = 0; i < poseUI.Length; i++)
@@ -43,11 +87,27 @@ public class Pose : MonoBehaviour
             poseUI[i].SetActive(false);
         }
     }
+    /// <summary> ポーズ画面表示 </summary>
     private void showUI()
     {
         for (int i = 0; i < poseUI.Length; i++)
         {
             poseUI[i].SetActive(true);
         }
+    }
+    /// <summary> セレクト画面に移動 </summary>
+    private void SelectMove()
+    {
+        SceneManager.LoadScene(SelectSceneIndex);
+    }
+    /// <summary> リスタートする </summary>
+    private void ReStartMove()
+    {
+        SceneManager.LoadScene(ReStartSceneIndex);
+    }
+    /// <summary> オプション画面に移動 </summary>
+    private void OptionMove()
+    {
+        SceneManager.LoadScene(OptionSceneIndex);
     }
 }
